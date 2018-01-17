8eaa4b1310f2efcb9a202ab59234fce83aea1726
Iarlen <iarlensilva775@gmail.com>
2018-01-16 22:24:17 -0300

    Criando e populando cursos

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
+  cursos: string[] = ['Angular2', 'Java', 'JS'];
+
   constructor() { }
 
   ngOnInit() {
