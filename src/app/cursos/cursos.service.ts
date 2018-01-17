f35d10b91a5ddd974084a26c88a16f2fe0f57d13
Iarlen <iarlensilva775@gmail.com>
2018-01-17 20:58:48 -0300

    Mostrando a quantidade que o serviço é usado

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -4,7 +4,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CursosService {
 
-  constructor() { }
+  constructor() {
+    console.log('CursosService');
+   }
 
   getCursos() {
     return ['Angular2', 'Java', 'JS'];
