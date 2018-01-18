431808cca35841be194ab8a45d32c9f2b178d908
Iarlen <iarlensilva775@gmail.com>
2018-01-17 21:54:00 -0300

    Movendo para os modulos locais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
+import { CursosModule } from './cursos/cursos.module';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CursosService } from './cursos/cursos.service';
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CriarCursoComponent } from './criar-curso/criar-curso.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent,
-    CriarCursoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CriarCursoModule,
+    CursosModule
   ],
   // providers: [
   //   CursosService
